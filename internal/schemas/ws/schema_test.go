004417ebfa8d3459e902bdc78cc40948c3d5a0de
AleksandrMatsko <a.matsko@g.nsu.ru>
2023-12-02 18:25:37 +0700

    test: add tests for deserializing MessageTaskAnswer

--- internal/schemas/ws/schema_test.go
+++ internal/schemas/ws/schema_test.go
@@ -25,3 +25,45 @@ func Test_MessageTaskAnswer_WithChoiceAnswer_Deserialized(t *testing.T) {
 		t.Fatalf("fail to deserialize MessageTaskAnswer with err: %v", err)
 	}
 }
+
+func Test_MessageTaskAnswer_WithTextAnswer_Deserialized(t *testing.T) {
+	jsonStr := `
+		{
+			"msg-id": 1,
+			"kind": "task-answer",
+			"time": 1701515325249,
+			"ready": true,
+			"task-idx": 0,
+			"answer": {
+				"type": "text",
+				"value": "hello world"
+			}
+		}
+	`
+	var a MessageTaskAnswer
+	err := json.Unmarshal([]byte(jsonStr), &a)
+	if err != nil {
+		t.Fatalf("fail to deserialize MessageTaskAnswer with err: %v", err)
+	}
+}
+
+func Test_MessageTaskAnswer_WithCheckedTextAnswer_Deserialized(t *testing.T) {
+	jsonStr := `
+		{
+			"msg-id": 1,
+			"kind": "task-answer",
+			"time": 1701515325249,
+			"ready": true,
+			"task-idx": 0,
+			"answer": {
+				"type": "checked-text",
+				"value": "hello world"
+			}
+		}
+	`
+	var a MessageTaskAnswer
+	err := json.Unmarshal([]byte(jsonStr), &a)
+	if err != nil {
+		t.Fatalf("fail to deserialize MessageTaskAnswer with err: %v", err)
+	}
+}
