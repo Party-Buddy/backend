b7131dd524175342b03b08978c1edc2c23d409c3
Alexander Yanin <a.yanin@g.nsu.ru>
2023-12-05 22:01:56 +0700

    feat: calculate player scores for plain tasks

--- internal/session/constants.go
+++ internal/session/constants.go
@@ -3,6 +3,13 @@ package session
 import "time"
 
 var (
-	NoOwnerTimeout     = time.Duration(5) * time.Minute
-	GameStartedTimeout = time.Duration(5) * time.Second
+	NoOwnerTimeout     = 5 * time.Minute
+	GameStartedTimeout = 5 * time.Second
+	TaskEndTimeout     = 10 * time.Second
+)
+
+// How many points players gain for correctly answering questions.
+var (
+	CheckedTextTaskPoints Score = 2
+	ChoiceTaskPoints      Score = 2
 )
