6eb6eca14a3429f04a181bbe9ce069b295b4e7ce
AleksandrMatsko <a.matsko@g.nsu.ru>
2023-12-05 22:32:53 +0700

    refactor: logging and error sending for image_handlers

--- internal/api/handlers/image_handlers.go
+++ internal/api/handlers/image_handlers.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"encoding/json"
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 	"image/jpeg"
@@ -17,24 +16,19 @@ type GetImageHandler struct{}
 // GetImageHandler get an image from fs.
 // Before reading file it uses r.Context() to get transaction and context to check if image is uploaded
 func (g GetImageHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	encoder := json.NewEncoder(w)
 	vars := mux.Vars(r)
 	val, ok := vars["img-id"]
 	if !ok {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusNotFound)
-		dto := api.Errorf(api.ErrNotFound, "img-id not provided")
-		_ = encoder.Encode(dto)
-		log.Printf("request: %v %s -> err: %v", r.Method, r.URL, dto)
+		msg := "img-id not provided"
+		WriteErrorResponse(w, http.StatusNotFound, api.ErrNotFound, msg)
+		log.Printf("request: %v %s -> err: %v", r.Method, r.URL, msg)
 		return
 	}
 	imgID, err := uuid.Parse(val)
 	if err != nil {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusNotFound)
-		dto := api.Errorf(api.ErrNotFound, "invalid url")
-		_ = encoder.Encode(dto)
-		log.Printf("request: %v %s -> err: %v", r.Method, r.URL, dto)
+		msg := "invalid url"
+		WriteErrorResponse(w, http.StatusNotFound, api.ErrNotFound, msg)
+		log.Printf("request: %v %s -> err: %v", r.Method, r.URL, msg)
 		return
 	}
 
@@ -43,32 +37,26 @@ func (g GetImageHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	imgMetadata, err := db.GetImageMetadataByID(tx, r.Context(), uuid.NullUUID{UUID: imgID, Valid: true})
 
 	if err != nil {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusNotFound)
-		dto := api.Errorf(api.ErrNotFound, "no record in db")
-		_ = encoder.Encode(dto)
-		log.Printf("request: %v %s -> err: %v", r.Method, r.URL, dto)
+		msg := "not found"
+		WriteErrorResponse(w, http.StatusNotFound, api.ErrNotFound, msg)
+		log.Printf("request: %v %s -> err: %v", r.Method, r.URL, msg)
 		return
 	}
 
 	// TODO: middleware.AuthInfoFromContext(r.Context())
 
 	if !imgMetadata.Uploaded {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusNotFound)
-		dto := api.Errorf(api.ErrNotFound, "image is not uploaded")
-		_ = encoder.Encode(dto)
-		log.Printf("request: %v %s -> err: %v", r.Method, r.URL, dto)
+		msg := "image is not uploaded"
+		WriteErrorResponse(w, http.StatusNotFound, api.ErrNotFound, msg)
+		log.Printf("request: %v %s -> err: %v", r.Method, r.URL, msg)
 		return
 	}
 
 	img, err := db.GetImageFromFS(imgMetadata.ID.UUID)
 	if err != nil {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusInternalServerError)
-		dto := api.Errorf(api.ErrInternal, "image not found in storage")
-		_ = encoder.Encode(dto)
-		log.Printf("request: %v %s -> err: %v", r.Method, r.URL, dto)
+		msg := "image not found in storage"
+		WriteErrorResponse(w, http.StatusInternalServerError, api.ErrInternal, msg)
+		log.Printf("request: %v %s -> err: %v", r.Method, r.URL, msg)
 		return
 	}
 
